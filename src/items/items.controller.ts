223a6a395942a9efb3d9c4e6d09b43debf51bb49
sc30gsw <haidoushi875@gmail.com>
2023-07-09 07:55:57 +0900

    DIの実装

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common'
+import { ItemsService } from './items.service'
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Get()
+  findAll() {
+    return this.itemsService.findAll()
+  }
+}
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  findAll() {
+    return 'This is ItemsService'
+  }
+}
