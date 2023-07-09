24af45eb53930c8bb5b85f243ae8f06e2368f8eb
sc30gsw <haidoushi875@gmail.com>
2023-07-09 09:05:54 +0900

    更新機能の実装(CRUDのU)

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common'
 import { ItemsService } from './items.service'
 import { Item } from './item.model'
 import { ItemStatus } from './item-status.enum'
@@ -33,5 +33,10 @@ export class ItemsController {
     }
 
     return this.itemsService.create(item)
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id)
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { Item } from './item.model'
+import { ItemStatus } from './item-status.enum'
 
 @Injectable()
 export class ItemsService {
@@ -15,6 +16,13 @@ export class ItemsService {
 
   create(item: Item): Item {
     this.items.push(item)
+
+    return item
+  }
+
+  updateStatus(id: string): Item {
+    const item = this.items.find((item) => item.id === id)
+    item.status = ItemStatus.SOLD_OUT
 
     return item
   }
