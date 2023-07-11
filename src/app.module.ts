9ab2bdd7be9046aec7ce50aa4555eb74094cb8f8
sc30gsw <haidoushi875@gmail.com>
2023-07-12 08:38:21 +0900

    DB接続設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common'
-import { ItemsModule } from './items/items.module';
+import { ItemsModule } from './items/items.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'flea-market',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
