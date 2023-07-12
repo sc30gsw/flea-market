bc82bfd2dafdcc9c51e819fadf349d118be12f9d
sc30gsw <haidoushi875@gmail.com>
2023-07-13 07:35:38 +0900

    モジュールの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { ItemsModule } from './items/items.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppDataSource } from './data-source'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ItemsModule, TypeOrmModule.forRoot(AppDataSource.options)],
+  imports: [ItemsModule, TypeOrmModule.forRoot(AppDataSource.options), AuthModule],
   controllers: [],
   providers: [],
 })
