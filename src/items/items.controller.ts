a5a526166f1eefdddeb163c97baf0afed2420841
sc30gsw <haidoushi875@gmail.com>
2023-07-13 08:48:19 +0900

    リクエストからユーザーの情報を取得

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -14,6 +14,8 @@ import { Item } from '@/entities/item.entity'
 import { CreateItemDto } from './dto/create-item.dto'
 import { DeleteResult, UpdateResult } from 'typeorm'
 import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard'
+import { GetUser } from '@/auth/decorator/get-user.decorator'
+import { User } from '@/entities/user.entity'
 
 @Controller('items')
 export class ItemsController {
@@ -31,7 +33,10 @@ export class ItemsController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
-  async create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+  async create(
+    @Body() createItemDto: CreateItemDto,
+    @GetUser() user: User,
+  ): Promise<Item> {
     return await this.itemsService.create(createItemDto)
   }
 
